a7a4d902251bbc709f35e9c86d682355b492d45b
CedricF67 <cedric.falda@gmail.com>
2019-02-13 19:49:03 +0100

    Implemented security and users management

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    // Connexion d'un utilisateur
+
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Récupération des erreurs de login, s'il y en as
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Dernier nom d'utilisateur entré par l'utilisateur
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        // Appel du template
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    // Mot de passe oublié
+
+    /**
+     * @Route ("/forgottenPassword", name="app_security_forgottenPassword")
+     */
+    public function forgottenPassword(Request $request, UserPasswordEncoderInterface $encoder, \Swift_Mailer $mailer, TokenGeneratorInterface $tokenGenerator): Response
+    {
+        // Si l'utilisateur à soumis le formulaire pour reset son mot de passe
+        if ($request->isMethod('POST')) {
+ 
+            // Récupération du nom de l'utilisateur dans la requête
+            $username = $request->request->get('username');
+ 
+            // Récupération de l'utilisateur correspondant dans la base de données
+            $entityManager = $this->getDoctrine()->getManager();
+            $user = $entityManager->getRepository(User::class)->findOneBy(['username' => $username]);
+ 
+            // Si l'utilisateur n'existe pas, une erreur est retournée
+            if ($user === null) {
+                $this->addFlash('danger', 'Utilisateur Inconnu');
+
+                // Redirection vers la liste des figures
+                return $this->redirectToRoute('app_trick_list');
+            }
+
+            // Génération d'un token pour l'utilisateur
+            $token = $tokenGenerator->generateToken();
+ 
+            // Tentative de lien entre le token et l'utilisateur
+            try{
+                $user->setResetToken($token);
+                $entityManager->flush();
+            } 
+            
+            // Affichage d'une éventuelle erreur
+            catch (\Exception $e) {
+                $this->addFlash('warning', $e->getMessage());
+
+                // Redirection vers la liste des figures
+                return $this->redirectToRoute('app_trick_list');
+            }
+ 
+            // Génération de l'url ou l'utilisateur devra se rendre pour reset son mot de passe
+            $url = $this->generateUrl('app_security_resetPassword', array('token' => $token), UrlGeneratorInterface::ABSOLUTE_URL);
+ 
+            // Génération du message qui sera envoyé à l'utilisateur
+            $message = (new \Swift_Message('Mot de passe oublié'))
+                ->setFrom('snowtricks@gmail.com')
+                ->setTo($user->getEmail())
+                ->setBody(
+                    "Voici un lien pour réinitialiser votre mot de passe : " . $url,
+                    'text/html'
+                );
+ 
+            // Envoi du message
+            $mailer->send($message);
+ 
+            $this->addFlash('notice', 'Mail envoyé');
+ 
+            // Redirection vers la liste des figures
+            return $this->redirectToRoute('app_trick_list');
+        }
+
+        // Génération du template
+        return $this->render('security/forgottenPassword.html.twig');
+    }
+
+    // Reset du mot de passe
+
+    /**
+     * @Route("/resetPassword/{token}", name="app_security_resetPassword")
+     */
+    public function resetPassword(Request $request, string $token, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        // Si l'utilisateur a soumis le formulaire avec son token de reset de mot de passe
+        if ($request->isMethod('POST')) {
+
+            // Stockage de l'appel du manager de doctrine
+            $entityManager = $this->getDoctrine()->getManager();
+ 
+            // Récupération de l'utilisateur correspondant au token
+            $user = $entityManager->getRepository(User::class)->findOneByResetToken($token);
+ 
+            // Si le token n'existe pas, une erreur est retournée
+            if ($user === null) {
+                $this->addFlash('danger', 'Token Inconnu');
+
+                // Redirection vers la liste des figures
+                return $this->redirectToRoute('app_trick_list');
+            }
+ 
+            // Suppression du token assigné à l'utilisateur
+            $user->setResetToken(null);
+
+            // Changement du mot de passe de l'utilisateur
+            $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('password')));
+            $entityManager->flush();
+ 
+            $this->addFlash('notice', 'Mot de passe mis à jour');
+ 
+            // Redirection vers la liste des figures
+            return $this->redirectToRoute('app_trick_list');
+        }else {
+            
+            // Génération du template
+            return $this->render('security/resetPassword.html.twig', ['token' => $token]);
+        }
+ 
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,206 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     * @Assert\NotBlank()
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(max=4096)
+     */
+    private $plainPassword;
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $createdDate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $avatar;
+
+    /**
+     * @var string le token qui servira lors de l'oubli de mot de passe
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $resetToken;
+
+    public function __construct()
+    {
+        $this->createdDate  = new \Datetime();
+        $this->roles = ['ROLE_USER'];
+        $this->avatar = 'default.png';
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(string $plainPassword): self
+    {
+        $this->plainPassword = $plainPassword;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        $this->plainPassword = null;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCreatedDate(): ?\DateTimeInterface
+    {
+        return $this->createdDate;
+    }
+
+    public function setCreatedDate(\DateTimeInterface $createdDate): self
+    {
+        $this->createdDate = $createdDate;
+
+        return $this;
+    }
+
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+
+    public function setAvatar($avatar = null): self
+    {
+        if (false === is_null($avatar)) {
+            $this->avatar = $avatar;
+        }
+        
+        return $this;
+    }
+
+    public function getResetToken(): string
+    {
+        return $this->resetToken;
+    }
+ 
+    public function setResetToken(?string $resetToken): void
+    {
+        $this->resetToken = $resetToken;
+    }
+}
